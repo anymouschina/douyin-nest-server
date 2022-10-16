c2fd102c2e52d715c32a100400d8c16652ac0339
libiqiang <624144061@qq.com>
2022-10-16 17:51:36 +0800

    perf

--- src/douyin_web_live/output/print.py
+++ src/douyin_web_live/output/print.py
@@ -13,22 +13,31 @@ RESET = Fore.RESET
 init()
 class Print(IOutput):
     def chat_output(self, msg):
-        print(f"\n{BLUE}[+] {msg} {RESET}")
+        print(msg)
+        # print(f"\n{BLUE}[+] {msg} {RESET}")
     def like_output(self, msg):
-        print(f"\n{CYAN}[+] {msg} {RESET}")
+        print(msg)
+        # print(f"\n{CYAN}[+] {msg} {RESET}")
     def member_output(self, msg):
-        print(f"\n{RED}[+] {msg} {RESET}")
+        print(msg)
+        # print(f"\n{RED}[+] {msg} {RESET}")
     def social_output(self, msg):
-        print(f"\n{GREEN}[+] {msg} {RESET}")
+        print(msg)
+        # print(f"\n{GREEN}[+] {msg} {RESET}")
 
     def gift_output(self, msg):
-        print(f"\n{MAGENTA}[+] {msg} {RESET}")
+        print(msg)
+        # print(f"\n{MAGENTA}[+] {msg} {RESET}")
 
     def userseq_output(self, msg):
-        print(f"\n{YELLOW}[+] {msg} {RESET}")
+        print(msg)
+
+        # print(f"\n{YELLOW}[+] {msg} {RESET}")
 
     def control_output(self, msg):
-        print(f"\n{CYAN}[+] {msg} {RESET}")
+        print(msg)
+        # print(f"\n{CYAN}[+] {msg} {RESET}")
 
     def fansclub_output(self, msg):
-        print(f"\n{GREEN}[+] {msg} {RESET}")
+        print(msg)
+        # print(f"\n{GREEN}[+] {msg} {RESET}")
